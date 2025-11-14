68190c5e9311a9a07f352e29c7b5b13ee17c282c
Iza-04 <Iza.m150492@gmail.com>
2025-11-14 19:20:23 +0500

    auth.js/router added

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,3 +1,6 @@
+import express from "express";
+const router = express.Router();
+
 export const registerUser = async (req, res) => {
   res.status(201).json({ message: "User registered (stub)" });
 };
@@ -9,3 +12,5 @@ export const loginUser = async (req, res) => {
 export const getUserProfile = async (req, res) => {
   res.json({ id: 1, name: "John Doe", role: "admin" });
 };
+
+export default router;
