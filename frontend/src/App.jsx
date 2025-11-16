fd3e0ef2fa367c2d5f984c8fba61f1c24225aee5
Iza-04 <Iza.m150492@gmail.com>
2025-11-16 16:12:02 +0500

    frontend/App.jsx/main.jsx changed totaly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-
-import { Routes, Route } from "react-router-dom";
-<Header />
-<Routes>
-  <Route path="/" element={<HomePage/>} />
-  <Route path="/inventory/:id/*" element={<InventoryPage/>} />
-  <Route path="/admin" element={<AdminPage/>} />
-  <Route path="/profile" element={<ProfilePage/>} />
-</Routes>
-
+import InventoryPage from "./pages/InventoryPage";
+import AdminPage from "./pages/AdminPage";
+import ProfilePage from "./pages/ProfilePage";
 
 export default function App() {
   return (
     <>
       <Header />
-      <HomePage />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/inventory/:id/*" element={<InventoryPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </>
   );
 }
