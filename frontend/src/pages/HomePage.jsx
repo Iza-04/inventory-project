fac08b18fe869797fa18795c2cc7d1930ed71302
Iza-04 <Iza.m150492@gmail.com>
2025-11-19 03:00:53 +0500

    changes added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
   return (
     <div>
-      <h3>Welcome</h3>
-      <p>This is a small inventory. Go to Inventory page to view items.</p>
+      <h3>Welcome colleagues</h3>
+      <p>Small inventory. Go to the Inventory page to view items.</p>
     </div>
   );
 }
